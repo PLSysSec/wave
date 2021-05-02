2fe029d06d95d596880d048156bf975f3e22ce47
Evan Johnson <e5johnso@eng.ucsd.edu>
2021-05-02 19:03:57 +0000

    update directory structure

---
+++ simple_fs/Cpp/wrappers.cpp
@@ -0,0 +1,56 @@
+#include <unistd.h>
+#include <sys/syscall.h>
+#include "wrappers_utils.h"
+#include "smack.h"
+
+//All arguments are the Wasm arguments
+int safe_open(const sandboxptr pathname, int flags){
+
+    hostptr host_pathname = path_from_sandbox(pathname);
+    if (host_pathname == NULL)
+        return -1;
+   
+    assert( (host_pathname >= (void*)membase) && (host_pathname + PATH_MAX <= (void*)(membase + memlen)) );
+    return syscall(SYS_open, 
+        host_pathname, 
+        flags, 
+        NULL);
+}
+
+int safe_close(int fd){
+    return syscall(SYS_close, 
+        fd, 
+        NULL);
+}
+
+ssize_t safe_read(int fd, sandboxptr buf, size_t count){
+    hostptr host_buf = sized_buf_from_sandbox(buf, count);
+    if (host_buf == NULL)
+        return -1;
+    
+    assert( (host_buf >= (void*)membase) && (host_buf + count <= (void*)(membase + memlen)) );
+    return syscall(SYS_read, 
+        fd, 
+        host_buf, 
+        count, 
+        NULL);
+}
+
+ssize_t safe_write(int fd, const sandboxptr buf, size_t count){
+    hostptr host_buf = sized_buf_from_sandbox(buf, count);
+    if (host_buf == NULL)
+        return -1;
+
+    assert( (host_buf >= (void*)membase) && (host_buf + count <= (void*)(membase + memlen)) );
+    return syscall(SYS_write, 
+        fd, 
+        host_buf, 
+        count, 
+        NULL);
+}
+
+/*
+int main(){
+
+}
+*/
---
+++ simple_fs/Rust/rust_wrappers/src/lib.rs
@@ -0,0 +1,52 @@
+extern crate smack;
+pub mod wrapper_utils;
+use smack::*;
+use wrapper_utils::*;
+
+
+//All arguments are the Wasm arguments
+int safe_open(const sandboxptr pathname, int flags){
+
+    hostptr host_pathname = path_from_sandbox(pathname);
+    if (host_pathname == NULL)
+        return -1;
+   
+    assert( (host_pathname >= (void*)membase) && (host_pathname <= (void*)(membase + memlen)) );
+    return syscall(SYS_open, 
+        host_pathname, 
+        flags, 
+        NULL);
+}
+
+int safe_close(int fd){
+    return syscall(SYS_close, 
+        fd, 
+        NULL);
+}
+
+ssize_t safe_read(int fd, sandboxptr buf, size_t count){
+    hostptr host_buf = sized_buf_from_sandbox(buf, count);
+    if (host_buf == NULL)
+        return -1;
+    
+    assert( (host_buf >= (void*)membase) && (host_buf <= (void*)(membase + memlen)) );
+    return syscall(SYS_read, 
+        fd, 
+        host_buf, 
+        count, 
+        NULL);
+}
+
+ssize_t safe_write(int fd, const sandboxptr buf, size_t count){
+    hostptr host_buf = sized_buf_from_sandbox(buf, count);
+    if (host_buf == NULL)
+        return -1;
+
+    assert( (host_buf >= (void*)membase) && (host_buf <= (void*)(membase + memlen)) );
+    return syscall(SYS_write, 
+        fd, 
+        host_buf, 
+        count, 
+        NULL);
+}
+
