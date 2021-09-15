c38e67bd59ebeb5e7ee6253d8cb8a11a1b41257a
Evan Johnson <e5johnso@eng.ucsd.edu>
2021-09-15 13:05:40 -0700

    reenable wasm2c frontend

--- src/lib.rs
+++ src/lib.rs
@@ -18,6 +18,7 @@ mod runtime;
 mod tests;
 mod trace;
 mod types;
-//#[cfg(not(feature = "verify"))]
-//pub mod wasm2c_frontend;
+#[cfg(not(feature = "verify"))]
+pub mod wasm2c_frontend;
 mod wrappers;
+
