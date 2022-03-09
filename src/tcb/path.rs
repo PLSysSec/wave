c1d8cbf09137efb16d6c619fa9583d190a5860fa
Evan Johnson <e5johnso@eng.ucsd.edu>
2022-03-08 19:17:29 -0800

    progress

--- src/tcb/path.rs
+++ src/tcb/path.rs
@@ -20,19 +20,19 @@ fn get_components(path: &PathBuf) -> Vec<Component> {
     path.components().collect()
 }
 
-#[trusted]
-#[requires(components_normalized(components))]
-#[ensures(old(is_relative(components)) ==> vec_is_relative(&result) )]
-#[ensures(old(min_depth(components)) == vec_depth(&result) )]
-fn from_components(components: &Vec<Component>) -> Vec<u8> {
-    //let p = p.as_path();
-    let mut path = PathBuf::new();
-    for c in components.iter() {
-        path.push(c.as_os_str());
-    } 
+// #[trusted]
+// #[requires(components_normalized(components))]
+// #[ensures(old(is_relative(components)) ==> vec_is_relative(&result) )]
+// #[ensures(old(min_depth(components)) == vec_depth(&result) )]
+// fn from_components(components: &Vec<Component>) -> Vec<u8> {
+//     //let p = p.as_path();
+//     let mut path = PathBuf::new();
+//     for c in components.iter() {
+//         path.push(c.as_os_str());
+//     } 
 
-    from_pathbuf(path)
-  }
+//     from_pathbuf(path)
+//   }
 
 // #[pure]
 // #[ensures()]
@@ -59,88 +59,47 @@ pub fn addr_matches_netlist_entry(netlist: &Netlist, addr: u32, port: u32, idx: 
 
 
 
-#[trusted]
-#[pure]
-#[requires(index < vec.len() )]
-pub fn lookup_component<'a>(
-    vec: &'a Vec<Component<'a>>,
-    index: usize,
-) -> &'a Component<'a> {
-    &vec[index]
-}
+// #[trusted]
+// #[pure]
+// #[requires(index < vec.len() )]
+// pub fn lookup_component<'a>(
+//     vec: &'a Vec<Component<'a>>,
+//     index: usize,
+// ) -> &'a Component<'a> {
+//     &vec[index]
+// }
 
 // If the first component is not the rootdir or a prefix (like Windows C://) its relative
 #[requires(c.len() > 0)]
 #[pure]
 #[trusted]
-#[ensures(result == true ==> components_normalized(c) )]
-fn is_relative(c: &Vec<Component>) -> bool {
-    let start = c[0];
+// #[ensures(result == true ==> components_normalized(c) )]
+fn is_relative(c: &OwnedComponents) -> bool {
+    let start = c.lookup(0);
     //let start = lookup_component(c, 0);
-    !(matches!(start, Component::Prefix(_) | Component::RootDir))
+    !(matches!(start, OwnedComponent::RootDir))
 }
-
-
-
-// #[trusted]
-// #[pure]
-// #[requires(index < vec.len() )]
-// pub fn vec_u8_lookup(
-//     vec: &Vec<u8>,
-//     index: usize,
-// ) -> u8 {
-//     vec[index]
-// }
-
-// #[pure]
-// fn pure_sum_h(v: &Vec<u8>, idx: usize, end: usize) -> u8 {
-//     if idx >= end || idx >= v.len() {
-//         return 0;
-//     }
-//     pure_sum_h(v, idx + 1, end) + vec_u8_lookup(v, idx)
-// }
-
-// #[pure]
-// fn pure_sum(v: &Vec<u8>) -> u8 {
-//     pure_sum_h(v, 0, v.len())
-// }
-
-// fn sum(v: &Vec<u8>) -> u8 {
-//     if v.len() <= 0 {
-//         return 0;
-//     }
-//     let mut acc = 0;
-//     let mut idx = 0;
-//     while idx < v.len() {
-//         body_invariant!(idx < v.len());
-//         // body_invariant!(acc == pure_sum_h(v, 0, idx));
-//         acc += vec_u8_lookup(v, idx);
-//         idx += 1;
-//     }
-//     acc
-// } 
-
 
 
 // use really big negative number instead of option because the verifier does not like returning options from pure code
 // apparently I can make it pure or I can make it untrusted but I cannot do both
 #[pure]
 #[trusted]
-#[requires(components_normalized(components))]
+// #[requires(components_normalized(components))]
 // #[ensures(result == pure_depth(components))]
-fn min_depth(components: &Vec<Component>) -> isize {
+fn min_depth(components: &OwnedComponents) -> isize {
     let mut curr_depth = 0;
     let mut idx = 0;
     while idx < components.len() {
         // invariant: idx < components.len()
         body_invariant!(curr_depth >= 0);
-        body_invariant!(components_normalized(components));
+        // body_invariant!(components_normalized(components));
         // pure_depth_h(components, 0)
-        match components[idx] {
-            Component::Prefix(_) | Component::RootDir => {return DEPTH_ERR;} // hacky, but fine for now
-            Component::CurDir => {},
-            Component::ParentDir => {curr_depth -= 1;},
-            Component::Normal(_) => {curr_depth += 1;},
+        match components.lookup(idx) {
+            OwnedComponent::RootDir => {return DEPTH_ERR;} // hacky, but fine for now
+            OwnedComponent::CurDir => {},
+            OwnedComponent::ParentDir => {curr_depth -= 1;},
+            OwnedComponent::Normal(_) => {curr_depth += 1;},
         };
         // if curr_depth ever dips below 0, it is illegal
         // this prevents paths like ../other_sandbox_home
@@ -150,53 +109,6 @@ fn min_depth(components: &Vec<Component>) -> isize {
         idx += 1;
     }
     curr_depth
-}
-
-#[cfg(feature = "verify")]
-predicate! {
-    pub fn components_normalized(components: &Vec<Component>) -> bool {
-        forall(|i: usize|
-            (i < components.len() ==> (
-                matches!(lookup_component(components, i), Component::CurDir | Component::ParentDir | Component::Normal(_))
-            ))
-        )
-    }
-}
-
-// #[ensures(result.is_ok() => components_normalized(components))]
-// fn normalize_path(components: &Vec<Component>) -> RuntimeResult<Vec<Component>> {
-//     let mut idx = 0;
-//     while idx < components.len() {
-//         if !(matches!(start, Component::Prefix(_) | Component::RootDir)) {
-
-//         }
-//     }
-//     !(matches!(start, Component::Prefix(_) | Component::RootDir))
-// }
-
-#[pure]
-#[requires(index < components.len())]
-#[requires(components_normalized(components))]
-#[trusted]
-// checks the correctness of
-fn pure_depth_h(components: &Vec<Component>, index: usize) -> isize {
-    if index >= components.len() {
-        0
-    }
-    else { 
-        let c = lookup_component(components, index);
-        pure_depth_h(components, index + 1) + elem_depth(c)
-    }
-}
-
-#[pure]
-#[requires(components_normalized(components))]
-fn pure_depth(components: &Vec<Component>) -> isize {
-    if components.len() <= 0 {
-        return 0;
-    }
-    // assert!(components_normalized(components));
-    pure_depth_h(components, 0)
 }
 
 #[pure]
@@ -224,25 +136,25 @@ fn vec_depth(components: &Vec<u8>) -> isize {
     panic!()
 }
 
-#[ensures(
-    match &result {
-        Ok(v) => vec_is_relative(&v) && (vec_depth(&v) >= 0),
-        _ => true,
-    }
-)]
+// #[ensures(
+//     match &result {
+//         Ok(v) => vec_is_relative(&v) && (vec_depth(&v) >= 0),
+//         _ => true,
+//     }
+// )]
 pub fn resolve_path(path: Vec<u8>) -> RuntimeResult<Vec<u8>> {
-    // let path = expand_path(&path, should_follow)?;
-    // let path = to_pathbuf(path);
-    let p = to_pathbuf(path);
-    let c: Vec<Component> = get_components(&p);
-    // let c: Vec<Component> = normalize_path(&c);
+    // let p = to_pathbuf(path);
+    // let c: Vec<Component> = get_components(&p);
+
+    // TODO: replace true with SHOULD_FOLLOW 
+    let c = expand_path(path, true)?;
 
     // depth < 0
     if c.len() <= 0 || !is_relative(&c) || min_depth(&c) < 0 {//|| depth(&c).is_some(){
         return Err(RuntimeError::Eacces);
     }
 
-    Ok(from_components(&c))
+    Ok(OwnedComponents::unparse(c))
     //Some(())
 }
 
@@ -320,65 +232,6 @@ fn maybe_expand_component(out_path: &mut OwnedComponents, comp: OwnedComponent, 
     
 }
 
-
-// #[trusted]
-// #[requires(!is_symlink(out_path) )]
-// #[ensures(!is_symlink(out_path))]
-// fn maybe_expand_component(out_path: &mut OwnedComponents, comp: OwnedComponent, num_symlinks: &mut isize) -> Option<OwnedComponents>{
-//     out_path.inner.push(comp);
-//     let linkpath = read_link_h(out_path)?;
-//     out_path.inner.pop(); // pop the component we just added, since it is a symlink
-//     *num_symlinks += 1;
-//     Some(linkpath)
-// }
-
-
-// #[ensures(x == old(x))]
-// fn structural_eq_test(x: &mut Vec<u8>) -> &Vec<u8>{
-//     x.push(3);
-//     x.pop();
-//     x
-// }
-
-// compute sum of `Vec<i32>`
-// #[pure]
-// #[trusted]
-// fn vec_sum(v: &Vec<i32>) -> i32 {
-//     let mut sum = 0;
-//     for x in v.iter() {
-//         sum += *x;
-//     }
-//     sum
-// }
-
-
-// fn sum_test1(){
-//     let x: Vec<u8> = Vec::new();
-//     let y: Vec<u8> = Vec::new();
-//     //assert!(x[..] == y[..]);
-// }
-
-// #[ensures(vec_sum(v) == old(vec_sum(v)))]
-// fn sum_test2(v: &mut Vec<i32>){
-//     v.push(3);
-//     v.pop();
-//     //assert!(x[..] == y[..]);
-// }
-
-
-
-// fn structural_eq_test2(){
-//     let x: Vec<u8> = Vec::new();
-//     let y: Vec<u8> = Vec::new();
-//     assert!(x[..] == y[..]);
-// }
-
-// returns None if there was some problem (should reject)
-// #[trusted]
-// #[ensures(result.is_some() && should_follow ==> no_symlinks(get_components(&result.unwrap())  ) )]
-// #[ensures(result.is_some() && !should_follow ==> no_symlinks(get_components(&result.unwrap()[..-1])))]
-
-
 // its an empty path, its not a symlink
 #[trusted]
 #[ensures(!is_symlink(&result))]
@@ -389,7 +242,7 @@ fn fresh_components() -> OwnedComponents {
 
 //#[ensures(!is_symlink(out_path))]
 
-fn expand_path(vec: Vec<u8>, should_follow: bool) -> Option<OwnedComponents> {
+fn expand_path(vec: Vec<u8>, should_follow: bool) -> RuntimeResult<OwnedComponents> {
     let p = to_pathbuf(vec);
     let components = get_components(&p);
 
@@ -416,11 +269,11 @@ fn expand_path(vec: Vec<u8>, should_follow: bool) -> Option<OwnedComponents> {
             expand_symlink(&mut out_path, linkpath, &mut num_symlinks);
         }
         if num_symlinks >= MAXSYMLINKS {
-            return None;
+            return Err(RuntimeError::Eloop);
         }
         idx += 1;
     }
-    Some(out_path)
+    Ok(out_path)
 }
 
 
--- src/tcb/verifier/external_specs/paths.rs
+++ src/tcb/verifier/external_specs/paths.rs
@@ -1,51 +1,52 @@
 use prusti_contracts::*;
+use owned_components::*;
 
-#[extern_spec]
-impl std::path::PathBuf {
-    #[pure]
-    // #[ensures(matches!(*self, Some(_)) == result)]
-    fn is_relative(&self) -> bool;
+// #[extern_spec]
+// impl std::path::PathBuf {
+//     #[pure]
+//     // #[ensures(matches!(*self, Some(_)) == result)]
+//     fn is_relative(&self) -> bool;
 
-    // #[pure]
-    // #[ensures(self.is_some() == !result)]
-    // fn is_none(&self) -> bool;
-}
+//     // #[pure]
+//     // #[ensures(self.is_some() == !result)]
+//     // fn is_none(&self) -> bool;
+// }
 
 
 
 #[extern_spec]
 impl OwnedComponents {
-    #[trusted]
-    #[ensures(result.len() == 0)]
-    pub fn new() -> Self;
+    #[pure]
+    fn len(&self) -> usize;
+
+    // #[ensures(result.len() == 0)]
+    pub fn new() -> OwnedComponents;
 
     //pub fn as_path(&self) -> &Path;
 
     //pub fn parse(p: PathBuf) -> Self;
 
-    #[trusted]
-    #[pure]
-    #[requires(index < self.len())]
+    // #[pure]
+    // #[requires(idx < self.len())]
     pub fn lookup(&self, idx: usize);
 
 
 
-    #[trusted]
-    #[ensures(self.len() == old(self.len()) + 1)]
-    #[ensures(self.lookup(old(self.len())) == old(value))]
-    #[ensures(forall(|i: usize| (i < old(self.len())) ==>
-                    self.lookup(i) == old(self.lookup(i))))]
-    pub fn push(&mut self, c: OwnedComponent);
+    // #[ensures(self.len() == old(self.len()) + 1)]
+    // #[ensures(self.lookup(old(self.len())) == old(value))]
+    // #[ensures(forall(|i: usize| (i < old(self.len())) ==>
+    //                 self.lookup(i) == old(self.lookup(i))))]
+    pub fn push(&mut self, value: OwnedComponent);
 
 
-    #[trusted]
-    #[requires(self.len() > 0)]
-    #[ensures(self.len() == old(self.len()) - 1)]
-    #[ensures(forall(|i: usize| (i < self.len()) ==>
-                    self.lookup(i) == old(self.lookup(i))))]
+    // #[requires(self.len() > 0)]
+    // #[ensures(self.len() == old(self.len()) - 1)]
+    // #[ensures(forall(|i: usize| (i < self.len()) ==>
+    //                 self.lookup(i) == old(self.lookup(i))))]
     pub fn pop(&mut self);
 
-    #[trusted]
-    #[pure]
-    pub fn len(&self);
+
+    
+    pub fn unparse(self) -> Vec<u8>;
+
 }
