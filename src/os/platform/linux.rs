3f90dcec033a2d4db4f93c0e1d00deb5d65a93c7
Evan Laufer <evan.m.laufer@gmail.com>
2022-03-16 14:54:15 -0700

    Cleanup merge and fix linux bugs

--- src/os/platform/linux.rs
+++ src/os/platform/linux.rs
@@ -5,11 +5,10 @@ use crate::tcb::os_specs::*;
 #[cfg(feature = "verify")]
 use crate::tcb::verifier::*;
 use crate::types::*;
-use crate::{effect, four_effects, no_effect, one_effect, three_effects, two_effects};
-use extra_args::with_ghost_var;
+use crate::{effect, effects};
 use prusti_contracts::*;
 use syscall::syscall;
-
+use wave_macros::with_ghost_var;
 
 #[with_ghost_var(trace: &mut Trace)]
 #[requires(ctx_safe(ctx))]
@@ -85,90 +84,3 @@ pub fn trace_allocate(ctx: &VmCtx, fd: HostFd, offset: i64, len: i64) -> Runtime
     let r = os_allocate(os_fd, offset, len);
     RuntimeError::from_syscall_ret(r)
 }
-//#[with_ghost_var(trace: &mut Trace)]
-//#[requires(trace_safe(trace, ctx.memlen) && ctx_safe(ctx))]
-//#[ensures(trace_safe(trace, ctx.memlen) && ctx_safe(ctx))]
-//#[ensures(one_effect!(old(trace), trace, effect!(FdAccess)))]
-//pub fn trace_advise(
-//    ctx: &VmCtx,
-//    fd: HostFd,
-//    offset: i64,
-//    len: i64,
-//    advice: i32,
-//) -> RuntimeResult<usize> {
-//    let os_fd: usize = fd.into();
-//    let r = os_advise(os_fd, offset, len, advice);
-//    RuntimeError::from_syscall_ret(r)
-//}
-//
-//#[with_ghost_var(trace: &mut Trace)]
-//#[requires(specs.capacity() >= 2)]
-//#[requires(trace_safe(trace, ctx.memlen) && ctx_safe(ctx))]
-//#[ensures(trace_safe(trace, ctx.memlen) && ctx_safe(ctx))]
-//#[ensures(one_effect!(old(trace), trace, effect!(FdAccess)))]
-//pub fn trace_futimens(
-//    ctx: &VmCtx,
-//    fd: HostFd,
-//    specs: &Vec<libc::timespec>,
-//) -> RuntimeResult<usize> {
-//    let os_fd: usize = fd.into();
-//    let r = os_futimens(os_fd, specs);
-//    RuntimeError::from_syscall_ret(r)
-//}
-//
-//#[with_ghost_var(trace: &mut Trace)]
-//#[requires(specs.capacity() >= 2)]
-//#[requires(trace_safe(trace, ctx.memlen) && ctx_safe(ctx))]
-//#[ensures(trace_safe(trace, ctx.memlen) && ctx_safe(ctx))]
-//#[ensures(two_effects!(old(trace), trace, effect!(FdAccess), effect!(PathAccess)))]
-//pub fn trace_utimensat(
-//    ctx: &VmCtx,
-//    fd: HostFd,
-//    pathname: SandboxedPath,
-//    specs: &Vec<libc::timespec>,
-//    flags: libc::c_int,
-//) -> RuntimeResult<usize> {
-//    let os_fd: usize = fd.into();
-//    let os_path: Vec<u8> = pathname.into();
-//    let r = os_utimensat(os_fd, os_path, specs, flags);
-//    RuntimeError::from_syscall_ret(r)
-//}
-//
-//#[with_ghost_var(trace: &mut Trace)]
-//#[requires(trace_safe(trace, ctx.memlen) && ctx_safe(ctx))]
-//#[ensures(trace_safe(trace, ctx.memlen) && ctx_safe(ctx))]
-//#[ensures(no_effect!(old(trace), trace))]
-//pub fn trace_clock_get_time(
-//    ctx: &VmCtx,
-//    clock_id: libc::clockid_t,
-//    spec: &mut libc::timespec,
-//) -> RuntimeResult<usize> {
-//    let r = os_clock_get_time(clock_id, spec);
-//    RuntimeError::from_syscall_ret(r)
-//}
-//
-//#[with_ghost_var(trace: &mut Trace)]
-//#[requires(trace_safe(trace, ctx.memlen) && ctx_safe(ctx))]
-//#[ensures(trace_safe(trace, ctx.memlen) && ctx_safe(ctx))]
-//#[ensures(no_effect!(old(trace), trace))]
-//pub fn trace_clock_get_res(
-//    ctx: &VmCtx,
-//    clock_id: libc::clockid_t,
-//    spec: &mut libc::timespec,
-//) -> RuntimeResult<usize> {
-//    let r = os_clock_get_res(clock_id, spec);
-//    RuntimeError::from_syscall_ret(r)
-//}
-//
-//#[with_ghost_var(trace: &mut Trace)]
-//#[requires(trace_safe(trace, ctx.memlen) && ctx_safe(ctx))]
-//#[ensures(trace_safe(trace, ctx.memlen) && ctx_safe(ctx))]
-//#[ensures(no_effect!(old(trace), trace))]
-//pub fn trace_nanosleep(
-//    ctx: &VmCtx,
-//    req: &libc::timespec,
-//    rem: &mut libc::timespec,
-//) -> RuntimeResult<usize> {
-//    let r = os_nanosleep(req, rem);
-//    RuntimeError::from_syscall_ret(r)
-//}
