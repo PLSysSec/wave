7738775ea6211613198db99233daface44e80157
Evan Johnson <e5johnso@eng.ucsd.edu>
2023-01-24 10:48:37 -0800

    first real test

--- src/tests/integration_tests.rs
+++ src/tests/integration_tests.rs
@@ -1,19 +1,25 @@
 use std::process::Command;
 
+// execute an example using our example/make infrastructure
+// returns stdout
+fn run_and_capture(example_path: &str) -> String {
+    let output = Command::new("make")
+    .arg("-s") // silent
+    .arg("-C") // change directory to example dir
+    .arg(example_path)
+    .arg("run")
+    .output()
+    .unwrap_or_else(|e| { panic!("failed to execute process: {}", e) });
+
+    match String::from_utf8(output.stdout) {
+        Ok(v) => v,
+        Err(e) => panic!("Invalid UTF-8 sequence: {}", e),
+    }
+}
 
 
 #[test]
 fn example_cat() {
-
-    // let output = Command::new("python.exe")
-    //     .arg("test.py")
-    //     .output()
-    //     .unwrap_or_else(|e| { panic!("failed to execute process: {}", e) });
-
-    // let s = match String::from_utf8(output.stdout) {
-    //     Ok(v) => v,
-    //     Err(e) => panic!("Invalid UTF-8 sequence: {}", e),
-    // };
-
-    // println!("result: {}", s); //must run "cargo test -- --nocapture" to see output
+    let s = run_and_capture("examples/cat");
+    assert!(s == "This is the contents of the file!\n");
 }
